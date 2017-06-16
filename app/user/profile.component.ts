ef7484ba93f8e5711ec0595250ed03bb73da5611
saibot777 <stefan.trajkovic@bluecouchdigital.com>
2017-06-16 14:39:49 +0200

    user authentication finished, logout added also

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -50,8 +50,17 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
     if(this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.toastr.success('Profile Saved');
+        .subscribe(() => {
+          this.toastr.success('Profile Saved');
+        })
+
     }
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/user/login']);
+    })
   }
 
 
